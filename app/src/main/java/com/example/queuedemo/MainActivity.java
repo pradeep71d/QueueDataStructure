de8e26bbdc6cd528e41d34aa55479e9c179c8db4
admin <pkdubey071@gmail.com>
2019-12-18 11:51:35 +0530

    add

--- app/src/main/java/com/example/queuedemo/MainActivity.java
+++ app/src/main/java/com/example/queuedemo/MainActivity.java
@@ -16,16 +16,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Queue<String> queue = new LinkedList<>();//quue implements linkedlist interface
-        queue.add("one");
+        queue.add("one");//adding elements in queue
         queue.add("two");
         queue.add("three");
         queue.add("four");
         System.out.println("out is=" + queue);
-        queue.remove("three");
+        queue.remove("three");//to remove particular element
         System.out.println("out is="+queue);
-        System.out.println("queue size is="+queue.size());
+        System.out.println("queue size is="+queue.size());// to find size of queue
         System.out.println("Queue Contains element 'two' or not? : " + queue.contains("two"));
-        queue.clear();
+        //queue.clear();
         System.out.println("out is="+queue);
+        System.out.println("peak can be as="+queue.peek());//used to see head of queue
+
     }
 }
