56f56cf69f353e6565b606e5082962d7a4592e91
admin <pkdubey071@gmail.com>
2019-12-18 11:57:30 +0530

    add

--- app/src/main/java/com/example/queuedemo/MainActivity.java
+++ app/src/main/java/com/example/queuedemo/MainActivity.java
@@ -18,16 +18,17 @@ public class MainActivity extends AppCompatActivity {
         Queue<String> queue = new LinkedList<>();//quue implements linkedlist interface
         queue.add("one");//adding elements in queue
         queue.add("two");
-        queue.add("three");
+        queue.add("three");//methods=add,remove,size,peak,poll
         queue.add("four");
         System.out.println("out is=" + queue);
         queue.remove("three");//to remove particular element
-        System.out.println("out is="+queue);
-        System.out.println("queue size is="+queue.size());// to find size of queue
+        System.out.println("out is=" + queue);
+        System.out.println("queue size is=" + queue.size());// to find size of queue
         System.out.println("Queue Contains element 'two' or not? : " + queue.contains("two"));
         //queue.clear();
-        System.out.println("out is="+queue);
-        System.out.println("peak can be as="+queue.peek());//used to see head of queue
-
+        System.out.println("out is=" + queue);
+        System.out.println("peak can be as=" + queue.peek());//used to see head of queue
+        System.out.println("out is=" + queue.poll());//used to remove peak
+        System.out.println("final out is="+queue);
     }
 }
